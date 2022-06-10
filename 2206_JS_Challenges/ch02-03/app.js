737187a84b5c4233a55f1de825825a38a6adf405
LimYeram <yeramyeye@gmail.com>
2022-06-10 11:33:52 +0900

    [5일차]ch3.6-7

--- 2206_JS_Challenges/ch02-03/app.js
+++ 2206_JS_Challenges/ch02-03/app.js
@@ -1,17 +1,21 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    title.getElementsByClassName.color = "blue";
+    if(h1.className === "active") {
+        h1.className = "";
+    } else {
+        h1.className = "active";
+    }
 }
 
-function handleMouseEnter() {
-    title.innerText = "mouse is here!";
-}
+// function handleMouseEnter() {
+//     title.innerText = "mouse is here!";
+// }
 
-function handleMouseLeave() {
-    title.innerText = "mouse is gone!";
-}
+// function handleMouseLeave() {
+//     title.innerText = "mouse is gone!";
+// }
 
 title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleace", handleMouseLeave);
+// title.addEventListener("mouseenter", handleMouseEnter);
+// title.addEventListener("mouseleace", handleMouseLeave);
