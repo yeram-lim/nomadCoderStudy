15eb7ba84bb60463c613d23d59ea3349eb7be8a4
LimYeram <yeramyeye@gmail.com>
2022-06-13 20:38:19 +0900

    [7일차]ch4.1

---
+++ 2206_JS_Challenges/ch04/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginBtnClick() {
+    const username = loginInput.ariaValueMax;
+}
