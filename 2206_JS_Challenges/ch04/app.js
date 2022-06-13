d8d0638373e3cd45cd8b0d571198ca022508cada
LimYeram <yeramyeye@gmail.com>
2022-06-13 21:05:50 +0900

    [7일차] ch04.3

--- 2206_JS_Challenges/ch04/app.js
+++ 2206_JS_Challenges/ch04/app.js
@@ -1,6 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
-    const username = loginInput.ariaValueMax;
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault;
 }
+
+function handleLinkClick(event) {
+    event.preventDefault;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
