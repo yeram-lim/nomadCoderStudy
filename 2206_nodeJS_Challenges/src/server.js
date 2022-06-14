761fd8f04e9d34cffc284f0558f5028b37f17f33
LimYeram <yeramyeye@gmail.com>
2022-06-14 20:38:50 +0900

    [1일차]ch1.3

--- 2206_nodeJS_Challenges/src/server.js
+++ 2206_nodeJS_Challenges/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, {WebSocketServer}  from "ws";
 import express from "express";
 import path from 'path';
 
@@ -11,4 +13,9 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); //http 서버를 만든다. 이렇게 해야 서버에 접근 가능
+const wss = new WebSocketServer({ server }); 
+// 웹소켓 서버를 만들고 http 서버와 함께 둘 다 작동시킨다., 웹소켓 서버만 구동시기고 싶으면 {server}인자를 안 넘겨주면 된다
+
+server.listen(3000, handleListen);
