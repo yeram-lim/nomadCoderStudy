a3040717b602874d461b720fc6180703b18117ec
LimYeram <yeramyeye@gmail.com>
2022-06-14 22:02:35 +0900

    [1일차]ch1.7

--- 2206_nodeJS_Challenges/src/public/js/app.js
+++ 2206_nodeJS_Challenges/src/public/js/app.js
@@ -1,1 +1,28 @@
-alert('hi');
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);//ws 서버로 접속한다.
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server!")
+}) //서버와 연결되면 발생한다. connection을 on 한다.
+
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+}); //메시지 이벤트, 메시지를 받으면 실행한다.
+  
+  socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+}); //서버와 연결이 끊기면 발생한다.
+  
+function handleSubmit(event) { 
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value); // 폼의 메시지를 서버로 보낸다.
+    input.value = "";
+}
+  
+messageForm.addEventListener("submit", handleSubmit); //폼의 제출 버튼을 누르면?
+
+  
--- 2206_nodeJS_Challenges/src/server.js
+++ 2206_nodeJS_Challenges/src/server.js
@@ -11,11 +11,30 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app); //http 서버를 만든다. 이렇게 해야 서버에 접근 가능
 const wss = new WebSocketServer({ server }); 
-// 웹소켓 서버를 만들고 http 서버와 함께 둘 다 작동시킨다., 웹소켓 서버만 구동시기고 싶으면 {server}인자를 안 넘겨주면 된다
+// 웹소켓 서버를 만들고 http 서버와 함께 둘 다 작동시킨다., 웹소켓 서버만 구동시기고 싶으면 {server}인자를 안 넘겨주면 된다   
+
+const sockets = [];
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+
+  socket.on("close", onSocketClose);
+
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    // 연결된 모든 소켓에 접근하기 위해 forEach를 사용한다. 크롬에게 받으면 크롬에게만 보내고 파이어 폭스에게 받으면 파이어폭스에게만 돌려보내는 것을 방지.
+  });
+});
 
 server.listen(3000, handleListen);
