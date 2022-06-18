62606778756dbcd05b6e66008280843cc8638602
LimYeram <yeramyeye@gmail.com>
2022-06-18 17:10:41 +0900

    [2일차] ch02

--- 2206_nodeJS_Challenges/src/public/js/app.js
+++ 2206_nodeJS_Challenges/src/public/js/app.js
@@ -1,12 +1,63 @@
 const socket = io(); //io는 백엔드와 자동적으로 연결해주는 함수이다. 이렇게만 쓰면 서버와 연결 완료
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const welcomeForm = welcome.querySelector("form");
 const room = document.getElementById("room");
+const user = document.getElementById("user");
 
 room.hidden = true;
+user.hidden = true;
 
 let roomName;
+let nickName;
+
+welcomeForm.addEventListener("submit", handleRoomSubmit);
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+
+  roomName = input.value;
+  socket.emit("enter_room", input.value, showRoom);
+  input.value = "";
+}
+
+function showRoom() {
+  // 방 메시지 보내는 창 나오게 함
+  welcome.hidden = true;
+  room.hidden = false;
+  user.hidden = false;
+
+  const roomNameBox = document.querySelector("#roomName");
+  roomNameBox.innerText = `Room ${roomName}`;
+
+  const msgForm = room.querySelector("#msg");
+  const nameForm = user.querySelector("#name");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nameForm.addEventListener("submit", handleNicknameSubmit);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
+socket.on("new_message", addMessage);
+//(msg) => {addMessage(msg)} 와 같음
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = user.querySelector("#name input");
+
+  socket.emit("nickname", input.value, showRoom);
+  nickName = input.value;
+  input.value = "";
+}
 
 function addMessage(message) {
     const ul = room.querySelector("ul");
@@ -15,33 +66,29 @@ function addMessage(message) {
     ul.appendChild(li);
 }
 
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, () => {
-      addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
-}
-
-
-socket.on("welcome", () => {
-    addMessage("someone joined!");
+socket.on("welcome", (user, newCount) => {
+  const h3 = document.querySelector("h3#roomName");
+  h3.innerText = `Room ${roomName} (${newCount})`;
+  addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", () => {
-    addMessage("someone left ㅠㅠ");
+socket.on("bye", (left, newCount) => {
+  addMessage(`${left} left ㅠㅠ`);
 });
 
-socket.on("new_message", addMessage);
-//(msg) => {addMessage(msg)} 와 같음
+
+socket.on("room_change", (rooms) => {
+  const roomList = document.querySelector("ul");
+  roomList.innerHTML = '';
+  if(rooms.length === 0) {
+    roomList.innerHTML = '';
+    return;
+  }
+  rooms.forEach(room => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
+
--- 2206_nodeJS_Challenges/src/server.js
+++ 2206_nodeJS_Challenges/src/server.js
@@ -23,22 +23,58 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+// ㅋㅋㅋㅋㅋ 요약: sids에는 개인방, rooms에는 개인방,공개방 다있음.
+// rooms가 sids를 포함한다 보면됨.
+// 그래서 공개방만 얻고 싶을때는 rooms에서 sids를 빼면 됨
+function publicRooms() {
+  const { 
+    sockets: { // sockets 안으로 들어가 
+      adapter: { sids, rooms }, // apdapter를 가지고 sids와 rooms 정보를 가져온다.
+    },
+  } = wsServer;  // wsServer 안에서
+
+  const publicRooms = []; // room들을 모아둔다.
+  rooms.forEach((_, key) => { // adapter를 이용해 socket id를 찾아 private room을 찾는다.
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  console.log('######3333', publicRooms)
+
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size; // size: 방의 인원수를 센다.
+}
+
 wsServer.on("connection", (socket) => { // 프론트와 연결한다. 소켓을 받는다.
+  socket["nickname"] = "Anon";
+
   socket.onAny((event) => {
+    // console.log(wsServer.sockets.adapter);
     console.log(`Socket Event: ${event}`);
   });
+
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));  // 하나의 소켓에만 메시지를 보냄
+    wsServer.sockets.emit("room_change", publicRooms());  // 모든 소켓에 메시지를 보냄
   });
+
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    );
   });
+
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
