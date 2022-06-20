9f276b1dc919499a123efc33f59f2293d8b0de41
LimYeram <yeramyeye@gmail.com>
2022-06-20 21:18:18 +0900

    [nextjs]ch1.3

--- 2206_nextjs_course/pages/about.js
+++ 2206_nextjs_course/pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Potato() {
-    return "about us";
+    return (
+        <div>
+            <NavBar></NavBar>
+            <h1>about us</h1>
+        </div>
+    );
 }
