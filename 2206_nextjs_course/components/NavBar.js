e335a7aec7555107288b31deb72feb2c8804dd75
LimYeram <yeramyeye@gmail.com>
2022-06-21 00:21:00 +0900

    [nextjs]1.4

--- 2206_nextjs_course/components/NavBar.js
+++ 2206_nextjs_course/components/NavBar.js
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../components/NavBar.module.css"
 
 export default function NavBar() {
     const router = useRouter();
 
     return (
     <nav>      
-        <Link href="/">
-            <a style={{ color : router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <Link href="/" >
+            <a className={
+                `${ styles.link } 
+                ${ router.pathname === "/" ? styles.active : "" }` 
+                }>Home</a>
         </Link>
         <Link href="/about">
-            <a style={{ color : router.pathname === "/about" ? "red" : "blue" }}>About</a>
+            <a className={[ 
+                styles.link,
+                router.pathname === "/about" ? styles.active : "" 
+            ].join(" ")
+        }>About</a>
         </Link>
+        {/* 클래스 2개를 한꺼번에 쓰기 위한 방법 */}
     </nav>
     );
 }
