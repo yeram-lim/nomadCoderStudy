6f1f53a5a092eb966e26a8937bce5b558198936a
LimYeram <yeramyeye@gmail.com>
2022-06-24 02:59:45 +0900

    [nextjs]2.3

--- 2206_nextjs_course/pages/index.js
+++ 2206_nextjs_course/pages/index.js
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-      (async () => {
-        const { results } = await (
-          await fetch("/api/movies")
-        ).json();
-        setMovies(results);
-      })();
-    }, []);
+export default function Home({ results }) {
+    // const [movies, setMovies] = useState();
+    // useEffect(() => {
+    //   (async () => {
+    //     const { results } = await (
+    //       await fetch("/api/movies")
+    //     ).json();
+    //     setMovies(results);
+    //   })();
+    // }, []);
 
   return (
     <div className="container">
       <Seo title="Home" />
-        {!movies && <h4>Loading...</h4>}
-        {movies?.map((movie) => (
+        {/* {!movies && <h4>Loading...</h4>} */}
+        {results?.map((movie) => (
             <div className="movie" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
             <h4>{movie.original_title}</h4>
@@ -46,3 +46,16 @@ export default function Home() {
     </div>
   );
 }
+
+// 이 함수는 이름이 고정이다. 다른 이름을 쓸 수 없다.
+// 서버에서 실행된다. 그리고 리턴하는 값을 page에게 준다.
+export async function getServerSideProps() {
+    const { results } = await (
+      await fetch(`http://localhost:3000/api/movies`)
+    ).json();
+    return {
+      props: {
+        results,
+      },
+    };
+  }
