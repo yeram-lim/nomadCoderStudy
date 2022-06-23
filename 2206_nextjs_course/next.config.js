094ec163f591b9f1ca8bf2d61aa5ede3b664f360
LimYeram <yeramyeye@gmail.com>
2022-06-24 02:38:16 +0900

    [nextjs]2.2

--- 2206_nextjs_course/next.config.js
+++ 2206_nextjs_course/next.config.js
@@ -1,6 +1,27 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //유저가 url이 변화하는 모습을 볼 수 있다.
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path*",
+        destination: "/form/:path*",
+        permanent: false,
+      }
+    ]
+  },
+  //유저가 url이 변하는 모습을 볼 수 없다.
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
