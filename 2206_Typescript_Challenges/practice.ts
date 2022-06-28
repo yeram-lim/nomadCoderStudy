dcbc19fc0192081197d4f54e2807963a98c0cd81
LimYeram <yeramyeye@gmail.com>
2022-06-28 22:46:05 +0900

    [ts]2.3

--- 2206_Typescript_Challenges/practice.ts
+++ 2206_Typescript_Challenges/practice.ts
@@ -1,21 +1,3 @@
-type Player = {
-  readonly name: string, //값 변경은 못한다.
-  age?: number
-}
+const Player : readonly [string, number, boolean] = ["yeram", 24, true]
 
-const yeram : Player = {
-  name: "yeram"
-}
-
-const sehyeon : Player = {
-  name: "sehyeon",
-  age: 26
-}
-
-// yeram.name = "sehyeon"
-
-function playerMaker(name: string) : Player {
-  return{
-    name
-  }
-}
+let a = [] 
