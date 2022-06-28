ad5d007a361757aa6fed75c178748c1fe2e68abc
LimYeram <yeramyeye@gmail.com>
2022-06-28 22:38:37 +0900

    [nextjs]

--- 2206_nextjs_course/pages/index.js
+++ 2206_nextjs_course/pages/index.js
@@ -6,36 +6,21 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id,title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}` //유저에게 보일 url -> 쿼리스트링이 url에 다 드러나는 것을 막는 것
-    );
+    router.push(`/movies/${title}/${id}`);
   };
 
   return (
     <div className="container">
       <Seo title="Home" />
         {results?.map((movie) => (
-          // 영화 이미지를 클릭했을 때 이동/
           <div  
             onClick={() => onClick(movie.id, movie.original_title)}
             className="movie" 
             key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           
-          {/* 영화 텍스트를 클릭했을 때 이동 */}
           <h4> 
-            <Link href={{
-                pathname: `/movies/${movie.id}`,
-                query: {title: movie.original_title,},
-              }}
-              as={`/movies/${movie.id}`}
-              >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               <a>{movie.original_title}</a>
             </Link>
             </h4>
