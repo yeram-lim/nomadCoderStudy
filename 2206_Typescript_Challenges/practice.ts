53b6434ad577262749e9ce3f932358f93f124778
LimYeram <yeramyeye@gmail.com>
2022-06-30 21:15:11 +0900

    [ts]3.0

--- 2206_Typescript_Challenges/practice.ts
+++ 2206_Typescript_Challenges/practice.ts
@@ -1,12 +1,24 @@
-// function add(a: number, b: number) {
-//   return a + b
-// }
+// 3.0
+function add(a: number, b: number) {
+  return a + b
+}
 
 // const add = (a: number, b: number) => {
 //   return a + b
 // }
 
-type Add = (a: number, b: number) => number;
+// type Add = (a: number, b: number) => number;
 
-const add: Add = (a, b) => a + b;
+// const add: Add = (a, b) => a + b;
 
+// 3.2
+// type SuperPrint = {
+//   <T>(arr: T[]): T //무엇이는 return할 수 있다.
+// }
+
+// const superPrint: SuperPrint = (arr) => arr[0]
+
+// superPrint([1, 2, 3, 4])
+// superPrint([true, false, true])
+// superPrint(['true', 'false', 'true']) // 모두 가능
+// superPrint([1, 2, true, false,'true', 'false', 'true']) 
