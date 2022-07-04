a3725bb70dd2e9fb7b9719abec595ba86ff35734
LimYeram <yeramyeye@gmail.com>
2022-07-04 21:49:58 +0900

    [ts]4.2

---
+++ 2206_Typescript_Challenges/ch04/2.ts
@@ -0,0 +1,17 @@
+interface User {
+  name: string
+}
+
+interface User {
+  lastName: string
+}
+
+interface User {
+  health: number
+}
+
+const yeram: User = {
+  name: "yeram",
+  lastName: "lim",
+  health: 10
+}
