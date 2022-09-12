be591c05b9274f14f21842928eb33477a2f622c6
LimYeram <yeramyeye@gmail.com>
2022-09-13 05:41:12 +0900

    [1.1-1.2] useInput

--- 2209_react_hooks/hook/src/useInput.js
+++ 2209_react_hooks/hook/src/useInput.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
 
-const useInput = (initalValue, validator) => {
+export const useInput = (initalValue, validator) => {
   const [value, setValue] = useState(initalValue);
   const onChange = (event) => {
     const {
@@ -18,16 +17,3 @@ const useInput = (initalValue, validator) => {
   }
   return {value, onChange}
 }
-
-function App() {
-  const maxLen = (value) => value.includes("@");
-  const name = useInput("Mr.", maxLen);
-  return (
-    <div className="App">
-      <h1>hello</h1>
-      <input placeholder='Name' {...name}></input>
-    </div>
-  );
-}
-
-export default App;
